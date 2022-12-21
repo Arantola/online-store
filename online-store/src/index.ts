2ac1e7103401c99e4b9a210063c9bd8c21e801bd
Arseny Fedorchenko <Arantola@mail.ru>
2022-12-21 18:21:57 +0300

    refactor: replace card styles into separate file

--- online-store/src/index.ts
+++ online-store/src/index.ts
@@ -4,6 +4,7 @@ import "./assets/styles/_catalog.css";
 import "./assets/styles/_cart.css";
 import "./assets/styles/_description.css";
 import "./assets/styles/_all.css";
+import "./assets/styles/_card.css";
 
 import App from "./modules/App";
 
