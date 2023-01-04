4b86b0d1b320d14ac3241a5ba4359bd7307f7782
Alex Romanchenko <monsters.alex0@gmail.com>
2023-01-04 18:37:00 +0300

    feat: product personal page info

--- online-store/src/pages/product.ts
+++ online-store/src/pages/product.ts
@@ -3,14 +3,14 @@ const product = `
                         <div class="breadcrumb">
                             <div><a href="../main/index.html">Store</a></div>
                             <span>>></span>
-                            <div>Theme</div>
+                            <div id="bread-theme">Theme</div>
                             <span>>></span>
-                            <div>Brand</div>
+                            <div id="bread-brand">Brand</div>
                             <span>>></span>
-                            <div>Name</div>
+                            <div id="bread-name">Name</div>
                         </div>
                         <div class="item">
-                            <div class="name">
+                            <div id="name-logo" class="name">
                                 <img src="../assets/images/en_2000.png" alt="name">
                             </div>
                             <div class="item-info">
@@ -23,39 +23,11 @@ const product = `
                                     </div>
                                 </div>
                                 <div class="characteristic">
-                                    <div class="detail">
-                                        <div class="">Price</div>
-                                        <div class="">151$</div>
-                                    </div>
-                                    <div class="detail">
-                                        <div class="">Designed by</div>
-                                        <div class="">Michael Kiesling</div>
-                                    </div>
-                                    <div class="detail">
-                                        <div class="">Published by</div>
-                                        <div class="">Year 2017</div>
-                                    </div>
-                                    <div class="detail">
-                                        <div class="">Developed by</div>
-                                        <div class="">thoun</div>
-                                    </div>
-                                    <div class="detail">
-                                        <div class="">Number of players</div>
-                                        <div class="">1-2</div>
-                                    </div>
-                                    <div class="detail">
-                                        <div class="">Theme</div>
-                                        <div class="">Theme name</div>
-                                    </div>
-                                    <div class="detail">
-                                        <div class="">Brand</div>
-                                        <div class="">Brand name</div>
-                                    </div>
                                 </div>
                             </div>
                             <div class="product-text"> 
                                 <h3>Description</h3>
-                                <p>Introduced by the Moors, azulejos (originally white and blue ceramic tiles) were fully embraced by the Portuguese when their king Manuel I, on a visit to the Alhambra palace in Southern Spain, was mesmerized by the stunning beauty of the Moorish decorative tiles. The king, awestruck by the interior beauty of the Alhambra, immediately ordered that his own palace in Portugal be decorated with similar wall tiles. As a tile-laying artist, you have been challenged to embellish the walls of the Royal Palace of Evora. In the game Azul, players take turns drafting colored tiles from suppliers to their player board. Later in the round, players score points based on how they've placed their tiles to decorate the palace. Extra points are scored for specific patterns and completing sets; wasted supplies harm the player's score. The player with the most points at the end of the game wins.</p>
+                                <p id="desc-text">Introduced by the Moors, azulejos (originally white and blue ceramic tiles) were fully embraced by the Portuguese when their king Manuel I, on a visit to the Alhambra palace in Southern Spain, was mesmerized by the stunning beauty of the Moorish decorative tiles. The king, awestruck by the interior beauty of the Alhambra, immediately ordered that his own palace in Portugal be decorated with similar wall tiles. As a tile-laying artist, you have been challenged to embellish the walls of the Royal Palace of Evora. In the game Azul, players take turns drafting colored tiles from suppliers to their player board. Later in the round, players score points based on how they've placed their tiles to decorate the palace. Extra points are scored for specific patterns and completing sets; wasted supplies harm the player's score. The player with the most points at the end of the game wins.</p>
                             </div>
                             <div class="pay-block">
                                 <button>Add to cart</button>
