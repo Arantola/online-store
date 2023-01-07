75d73f2094baa0ef967fde5913a29061731f5642
Alex Romanchenko <monsters.alex0@gmail.com>
2023-01-07 20:45:56 +0300

    feat: working promocode, fast cart btn, add bth in product

--- online-store/src/index.ts
+++ online-store/src/index.ts
@@ -8,11 +8,5 @@ import "./assets/styles/_card.css";
 import "./assets/styles/_modal.css";
 import App from "./modules/App";
 
-import rs_school from "./assets/icons/rs_school_js.svg";
-
 const app = new App();
 app.init();
-
-const RssLogo = document.querySelector<HTMLElement>(".link_RSSchool");
-if (RssLogo != undefined)
-  RssLogo.innerHTML = `<img src="${rs_school}" alt="rs_school">`;
